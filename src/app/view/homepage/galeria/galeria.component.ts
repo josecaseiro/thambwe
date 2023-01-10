03961ecd5ee03613fbbcb45d4695bd7e6a9c010d
Higino Denilson Januário <Higino502011@hotmail.com>
2023-01-10 22:06:08 +0100

    de

--- src/app/view/homepage/galeria/galeria.component.ts
+++ src/app/view/homepage/galeria/galeria.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Galeria } from 'src/app/models/galeria';
 import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -8,10 +11,28 @@ import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstra
 })
 export class GaleriaComponent implements OnInit {
   closeResult = '';
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal, private http: HttpClient) { }
 
   ngOnInit(): void {
+	this.proImagem()
   }
+  imagem: Galeria[] = []
+  url(valor:string | null){
+	if (valor == null) return null;
+	return environment.fileUrl+valor
+  }
+
+  
+proImagem(){
+	this.http.get<{img:Galeria[]}>(environment.apiUrl + 'galeria').subscribe({
+		next: (res)=>{
+			this.imagem = res.img
+			console.log(res.img)
+		}
+	})
+}
+
+
   open(content: any) {
 		this.modalService.open(content, {size: 'lg', ariaLabelledBy: 'modal-basic-title' }).result.then(
 			(result) => {
